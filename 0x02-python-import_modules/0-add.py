833d1404823504fea154ef866125ba14e8da2bc2
Cmadom <cmadom@st.ug.edu.gh>
2023-08-11 05:02:10 -0700

    Python - import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
-def add(a, b):
-    return a + b
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-from add_0 import add
-
-def main():
     a = 1
     b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
-
-if __name__ == "__main__":
-    main()
+    print("{} + {} = {}".format(a, b, add(a, b)))
