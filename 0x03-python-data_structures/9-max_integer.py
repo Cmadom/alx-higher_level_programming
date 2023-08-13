4d8e81afb25327c0ce9d868c12aabc519936fcba
Cmadom <cmadom@st.ug.edu.gh>
2023-08-13 03:53:47 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,13 +2,12 @@
 # 9-max_integer.py
 
 def max_integer(my_list=[]):
-    """Find the biggest integer of a list."""
-    if len(my_list) == 0:
-        return (None)
-
-    big = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > big:
-            big = my_list[i]
-
-            return (big)
+    if not my_list:
+        return None
+    
+    max_num = my_list[0]
+    for num in my_list:
+        if num > max_num:
+            max_num = num
+    
+    return max_num
