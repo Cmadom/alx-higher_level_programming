34c23af789bef212e4c02d1753a8f489057f5975
Cmadom <cmadom@st.ug.edu.gh>
2023-08-13 03:16:30 -0700

    updated

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # 0-print_list_integer.py
 
-def element_at(my_list, idx):
-    """Retrive an element from a list."""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return None
-    return (my_list[idx])
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print("{:d}".format(num))
+
+my_list = [10, 20, 30, 40, 50]
+print_list_integer(my_list)
